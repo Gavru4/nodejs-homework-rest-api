70546a822b8ea31715ff24dbc0c05ea3f32cb07e
Roman Havryliuk <gavru4r@gmail.com>
2022-05-03 12:22:57 +0300

    finished nw-2

--- server.js
+++
@@ -1,9 +0,0 @@
-const app = require("./app");
-
-const PORT = process.env.PORT;
-
-app.listen(PORT, (err) => {
-  if (err) console.error("Error at server launch:", err);
-
-  console.log(`Server running. Use our API on port:${PORT}`);
-});
