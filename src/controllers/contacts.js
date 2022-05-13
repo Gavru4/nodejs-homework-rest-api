c2b8cf859cb7d58a0297e08c788f9d93f36ce734
Roman Havryliuk <gavru4r@gmail.com>
2022-05-13 12:04:38 +0300

    changed the logic of accessing the database when searching for contacts by id

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -40,48 +40,74 @@ const deleteContact = async (req, res) => {
 const putContact = async (req, res) => {
   const { contactId } = req.params;
   const { name, email, phone, favorite } = req.body;
-  const contactById = await getContactById(contactId);
-  if (!contactById) {
-    return res.status(404).json({ status: "Not found" });
-  } else {
-    const updateContactItem = await updateContact(contactId, {
-      name,
-      email,
-      phone,
-      favorite,
-    });
-    res.status(200).json({ status: "success", updateContactItem });
-  }
+  // const contactById = await getContactById(contactId);
+  // if (!contactById) {
+  //   return res.status(404).json({ status: "Not found" });
+  // } else {
+  //   const updateContactItem = await updateContact(contactId, {
+  //     name,
+  //     email,
+  //     phone,
+  //     favorite,
+  //   });
+  //   res.status(200).json({ status: "success", updateContactItem });
+  // }
+
+  const updateContactItem = await updateContact(contactId, {
+    name,
+    email,
+    phone,
+    favorite,
+  });
+  res.status(200).json({ status: "success", updateContactItem });
+
+  if (!updateContactItem) res.status(404).json({ status: "Not found" });
 };
 
 const patchContact = async (req, res) => {
   const { contactId } = req.params;
   const { name, email, phone, favorite } = req.body;
-  const contactById = await getContactById(contactId);
-  if (!contactById) {
-    return res.status(404).json({ status: "Not found" });
-  } else {
-    const updateContactItem = await updateContact(contactId, {
-      name,
-      email,
-      phone,
-      favorite,
-    });
-    res.status(200).json({ status: "success", updateContactItem });
-  }
+  // const contactById = await getContactById(contactId);
+  // if (!contactById) {
+  //   return res.status(404).json({ status: "Not found" });
+  // } else {
+  //   const updateContactItem = await updateContact(contactId, {
+  //     name,
+  //     email,
+  //     phone,
+  //     favorite,
+  //   });
+  //   res.status(200).json({ status: "success", updateContactItem });
+  // }
+  // const contactById = await getContactById(contactId);
+  const updateContactItem = await updateContact(contactId, {
+    name,
+    email,
+    phone,
+    favorite,
+  });
+  res.status(200).json({ status: "success", updateContactItem });
+
+  if (!updateContactItem) res.status(404).json({ status: "Not found" });
 };
 
 const updateStatusContact = async (req, res) => {
   const { contactId } = req.params;
   const { favorite } = req.body;
-  const contactById = await getContactById(contactId);
+  // const contactById = await getContactById(contactId);
+  // if (!contactById) {
+  //   return res.status(404).json({ status: "Not found" });
+  // } else {
+  //   const updateContactItem = await updateContact(contactId, { favorite });
+  //   res.status(200).json({ status: "success", updateContactItem });
+  // }
 
-  if (!contactById) {
-    return res.status(404).json({ status: "Not found" });
-  } else {
-    const updateContactItem = await updateContact(contactId, { favorite });
-    res.status(200).json({ status: "success", updateContactItem });
-  }
+  const updateContactItem = await updateContact(contactId, { favorite });
+  res.status(200).json({ status: "success", updateContactItem });
+
+  console.log(updateContactItem);
+
+  if (!updateContactItem) res.status(404).json({ status: "Not found" });
 };
 
 module.exports = {
