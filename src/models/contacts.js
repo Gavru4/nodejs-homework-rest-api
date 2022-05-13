f5502fb119613b1a38799c7226eecf6b5c00a1e4
Roman Havryliuk <gavru4r@gmail.com>
2022-05-13 16:03:04 +0300

    fix catch errors func

--- src/models/contacts.js
+++ src/models/contacts.js
@@ -1,53 +1,24 @@
 const { Contact } = require("../db/contactsModel");
 
-const listContacts = async () => {
-  try {
-    const data = await Contact.find();
-    return data;
-  } catch (error) {
-    console.error(error.message);
-  }
-};
+const listContacts = async () => await Contact.find();
 
-const getContactById = async (contactId) => {
-  try {
-    const contactById = Contact.findById(contactId);
-    return await contactById;
-  } catch (error) {
-    console.error(error.message);
-  }
-};
+const getContactById = async (contactId) => await Contact.findById(contactId);
 
-const removeContact = async (contactId) => {
-  try {
-    const newContacts = Contact.findByIdAndDelete({ _id: contactId });
-    return await newContacts;
-  } catch (error) {
-    console.error(error.message);
-  }
-};
+const removeContact = async (contactId) =>
+  await Contact.findByIdAndDelete({ _id: contactId });
 
 const addContact = async (name, email, phone, favorite) => {
-  try {
-    const contact = new Contact({ name, email, phone, favorite });
-    return await contact.save();
-  } catch (error) {
-    console.error(error.message);
-  }
+  const contact = new Contact({ name, email, phone, favorite });
+
+  return await contact.save();
 };
-
 const updateContact = async (contactId, body) => {
-  try {
-    const { name, email, phone } = body;
+  const { name, email, phone } = body;
+  const updateContactById = Contact.findByIdAndUpdate(contactId, {
+    $set: { name, email, phone },
+  });
 
-    const updateContactById = Contact.findByIdAndUpdate(contactId, {
-      $set: { name, email, phone },
-    });
-
-    return await updateContactById;
-  } catch (error) {
-    console.error(error.message);
-  }
+  return await updateContactById;
 };
 
 module.exports = {
